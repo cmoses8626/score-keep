3859d2ea9829ee6fa4efbd71a39e7c042fb71232
Chris Moses <cmosesatmit@gmail.com>
2018-08-14 17:13:34 -0400

    Refactored Player component to use Material-UI Delete icon

--- imports/ui/Player.js
+++ imports/ui/Player.js
@@ -1,36 +1,54 @@
 import React from 'react';
 import {Players} from './../api/players'
 import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import IconButton from '@material-ui/core/IconButton';
+import DeleteIcon from '@material-ui/icons/Delete';
 
-export default class Player extends React.Component {
-  render() {
-    let itemClassName = `item item--position-${this.props.player.rank}`;
+const styles = theme => ({
+  button: {
+    margin: theme.spacing.unit,
+  },
+  input: {
+    display: 'none',
+  },
+});
 
-    return (
-      <div key={this.props.player._id} className={itemClassName}>
-        <div className='player'>
-          <div>
-            <p className='player__name'>{this.props.player.name}</p>
-            <p className='player__stats'>
-              {this.props.player.position} place: {this.props.player.score} point(s)
-            </p>
-          </div>
-          <div className='player__actions'>
-            <button className='button button--round' onClick={() => {
-              Players.update(this.props.player._id, {$inc: {score: -1}});
-            }}>-1</button>
-            <button className='button button--round' onClick={() => {
-              Players.update(this.props.player._id, {$inc: {score: 1}});
-            }}>+1</button>
-            <button className='button button--round' onClick={() => {
-              Players.remove(this.props.player._id)}}>X</button>
-            </div>
+function Player(props) {
+  const { classes, player } = props;
+  let itemClassName = `item item--position-${player.rank}`;
+
+  return (
+    <div key={player._id} className={itemClassName}>
+      <div className='player'>
+        <div>
+          <p className='player__name'>{player.name}</p>
+          <p className='player__stats'>
+            {player.position} place: {player.score} point(s)
+          </p>
+        </div>
+        <div className='player__actions'>
+          <button className='button button--round' onClick={() => {
+            Players.update(player._id, {$inc: {score: -1}});
+          }}>-1</button>
+          <button className='button button--round' onClick={() => {
+            Players.update(player._id, {$inc: {score: 1}});
+          }}>+1</button>
+          <IconButton
+            className={classes.button}
+            aria-label="Delete"
+            onClick={() => Players.remove(player._id)}>
+            <DeleteIcon />
+          </IconButton>
           </div>
         </div>
-      )
+      </div>
+    )
   }
-};
 
 Player.propTypes = {
-  player: PropTypes.object.isRequired
+  player: PropTypes.object.isRequired,
+  classes: PropTypes.object.isRequired
 };
+
+export default withStyles(styles)(Player);
